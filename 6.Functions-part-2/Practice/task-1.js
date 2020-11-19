5bf2ddd007a6f78a6f47ce7042afab9c5ba168e1
tamunaodisharia <tamuna.odisharia7@gmail.com>
2020-11-20 00:05:13 +0400

    feat(6.Functions-part-2): Finish practice

---
+++ 6.Functions-part-2/Practice/task-1.js
@@ -0,0 +1,34 @@
+// #### Task 1
+
+// Write your own implementation of function `mix`. 
+// This function is expecting unlimited number of parameters. 
+// Each parameter has to be a function, else throw error. Function `mix` invokes callbacks one by one. 
+// Parameter of each callback is the result of the previous callback.
+// First callback doesn't get any parameter.
+
+function mix(...args){
+    let result;
+    for(let i = 0; i < args.length; i++){
+        if(typeof args[i] !== 'function'){
+            throw new Error("parameters of mix() can only be fuctions");
+        }
+        result = args[i](result);
+    }
+    return result;
+}
+
+let myVar;
+try{
+    myVar = mix(() => {
+        return 0;
+    }, (prev) => {
+        return prev + 1;
+    }, (prev) => {
+        return prev * 2;
+    }) 
+   
+    console.log(myVar); // 2
+} catch(e) {
+    console.log("Error: " + e.message);
+}
+

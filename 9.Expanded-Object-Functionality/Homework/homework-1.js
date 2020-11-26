dcab3360625e1c0d5523ff3421af139e145b79a5
tamunaodisharia <tamuna.odisharia7@gmail.com>
2020-11-26 21:08:28 +0400

    feat(9.Expanded-Object-Functionality): finish hw

---
+++ 9.Expanded-Object-Functionality/Homework/homework-1.js
@@ -0,0 +1,49 @@
+// #### Task 1
+
+// Add the non-enumerable method `mergeDeepRight()` to `Object.prototype`.
+// This method extends the source object by copying properties from the object passed in the argument.
+// This method copies only the value of the properties, but does not copy the attributes (descriptors).
+
+// Only own properties are copied from the object in the argument
+// (even those that are not available for enumeration).
+
+// If there are properties that already exist in the source object,
+// their values are replaced by the values from the object passed in the argument.
+
+// If a property is object, it must also be copied along with nested data, nesting can be any.
+
+Object.defineProperty(Object.prototype, 'mergeDeepRight',{
+    value:function(obj){
+        Object.assign(this, obj);
+    },
+    enumerable: false
+    
+});
+
+const data = {
+    name: 'fred',
+    age: 10,
+    contact: {
+        email: 'moo@example.com',
+        meta: {
+            verified: true,
+            tags: ['important']
+        }
+    }
+};
+console.log(data);
+data.mergeDeepRight({
+    age: 40,
+    contact: {
+        email: 'baa@example.com',
+        favorite: true,
+        meta: {
+            tags: ['vip']
+        }
+    }
+});
+console.log(data);
+
+
+
+

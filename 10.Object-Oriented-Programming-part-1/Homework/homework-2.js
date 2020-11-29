555ef717d2b4ea4e6af3f25a2fadf25ef4db742a
tamunaodisharia <tamuna.odisharia7@gmail.com>
2020-11-30 03:02:45 +0400

    10.(OOP-part-1):Finish homework

---
+++ 10.Object-Oriented-Programming-part-1/Homework/homework-2.js
@@ -0,0 +1,58 @@
+// ## Add method isRunning
+// From the external code, we would like to be able to understand whether the coffee maker is running or not.
+
+// To do this, add a public `isRunning()` method to the coffee maker, which will return true if it is running and false if not.
+
+// Next code should work for you: 
+
+function CoffeeMachine(power, capacity) {
+	var waterAmount = 0;
+	var WATER_HEAT_CAPACITY = 4200;
+	var timer = 0;
+    function getTimeToBoil() {
+		return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+    }
+	
+    this.setWaterAmount = function(amount) {
+		if (amount < 0) {
+			throw new Error("Value has to be positive");
+		}
+		if (amount > capacity) {
+			throw new Error("You can't put more water, than " + capacity);
+		}
+		waterAmount = amount;
+	};
+	
+    function onReady() {
+        console.log('Coffee is ready');
+    }
+    this.setOnReady = function(func) {
+        onReady = func;
+    }
+    function setTimer(){
+        timer = 0;
+    }
+	this.run = function() {
+        timer = setTimeout(onReady, getTimeToBoil());
+        setTimeout(setTimer, getTimeToBoil());
+    };
+    this.isRunning = function() {
+        return timer !== 0;
+    }
+   
+}
+
+var coffeeMachine = new CoffeeMachine(20000, 500);
+coffeeMachine.setWaterAmount(100);
+
+console.log('Before: ' + coffeeMachine.isRunning()); // Before: false
+
+coffeeMachine.run();
+
+console.log('In progress: ' + coffeeMachine.isRunning()); // In progress: true
+
+coffeeMachine.setOnReady(function() {
+	console.log('After: ' + coffeeMachine.isRunning()); // After: false
+});
+coffeeMachine.run();
+
